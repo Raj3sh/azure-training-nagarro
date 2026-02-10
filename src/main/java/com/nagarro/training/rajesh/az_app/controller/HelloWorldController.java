1d9558b71879d0b1906bf6cee8bd3f422df1ce4d
Rajesh Das <rajesh.das01@nagarro.com>
2026-02-10 11:26:51 +0530

    Testing the environment variable per deployment stage

--- src/main/java/com/nagarro/training/rajesh/az_app/controller/HelloWorldController.java
+++ src/main/java/com/nagarro/training/rajesh/az_app/controller/HelloWorldController.java
@@ -1,13 +1,17 @@
 package com.nagarro.training.rajesh.az_app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${azure.app.service.env.variable}")
+    private String envVariable;
+
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World from environment variable: " + envVariable;
     }
 }
